580d8c966839bef69bcd1ecac4ec72fd846fe3fe
danijelkelava <dykyprod@gmail.com>
2018-05-17 15:35:42 +0200

    routing test

--- app/controllers/TriangleController.php
+++ app/controllers/TriangleController.php
@@ -10,16 +10,18 @@ class TriangleController extends Controller
 	private $data = [];
 
 	public function __construct()
-	{
-		$this->triangle = new Triangle('triangle', 3, 4, 5);
-		$this->data['type'] = $this->triangle->getType();
-		$this->data['circumference'] = $this->triangle->circumference();		
+	{		
 	}
 
 	public function index($a = '', $b = '', $c = '')
 	{
-		echo $a . ' ' . $b . ' ' . $c;
-		//echo json_encode($this->data);
+		if (is_numeric($a) && is_numeric($b) && is_numeric($c)) {
+			$triangle = new Triangle('triangle', $a, $b, $c);
+			$this->data['type'] = $triangle->getType();
+			$this->data['circumference'] = $triangle->circumference();
+			echo json_encode($this->data);
+		}
+		
 	}
 
 }
